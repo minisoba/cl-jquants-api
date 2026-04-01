1f5db8a2f163b64158f7b12e845b976bf87dcf7f
minisoba <minisoba@gmail.com>
2026-04-02 01:15:57 +0800

    Fix get-topix-prices crash: propagate HTTP errors instead of swallowing them
    perform-http-request was silently catching all errors and returning nil.
    The caller (%make-jquants-instance) then called gethash on nil, causing
    a confusing "NIL is not of type HASH-TABLE" TYPE-ERROR.
    
    Now HTTP errors propagate to the caller with clear messages like
    "HTTP Error 404 Not Found: ..." instead of a cryptic type error.
    Also added nil-safety in %make-jquants-instance as a defensive measure.
    
    Co-authored-by: minisoba <3029128+minisoba@users.noreply.github.com>

--- src/util.lisp
+++ src/util.lisp
@@ -48,6 +48,7 @@
      (loop
        with pagination-key
        do (let ((response (perform-http-request url :method :get)))
+            (unless response (return))
             (setf pagination-key (gethash "pagination_key" response))
             (dolist (hash-table (gethash ,node response))
               (push (make-jquants-instance-from-hash-table
